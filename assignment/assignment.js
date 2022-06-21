6d82e2dd4f36c554f1eae82a219f85f43c5a9acf
NC Das <nimai.iyf@gmail.com>
2022-06-21 23:49:35 +0600

    another operator worked on

--- assignment/assignment.js
+++ assignment/assignment.js
@@ -4,3 +4,9 @@ let x, y, sum;
     y = 5; 
     sum = x + y;
     console.log(sum);
+// Another operator
+let totalBook, book1, book2;
+    book1 = 100;
+    book2 = 50;
+    totalBook = book1 - book2;
+    console.log(totalBook);
