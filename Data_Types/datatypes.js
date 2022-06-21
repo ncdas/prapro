e23d911a668fd5bc6b092b762a9aa2b56bd8344f
NC Das <nimai.iyf@gmail.com>
2022-06-22 00:47:06 +0600

    write a simple array

--- Data_Types/datatypes.js
+++ Data_Types/datatypes.js
@@ -1,6 +1,7 @@
 
-// quotes
-let x = "This is Mr. x", y = '"This is Mr. Y"', z = "That's Mr. Z"; 
+// array
 
-    console.log(x + y + z);
+const asia = [ "BD", "In", "pak"  ]
+
+    console.log(asia);
 
