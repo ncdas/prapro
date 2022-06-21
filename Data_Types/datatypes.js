c33b647155ba11f6102295efe3075c094424722e
NC Das <nimai.iyf@gmail.com>
2022-06-22 00:27:48 +0600

    checked js execution sequencce

--- Data_Types/datatypes.js
+++ Data_Types/datatypes.js
@@ -1,5 +1,5 @@
 
 // adding number and String
-let firstName = "NC", age = 30; 
-    chkdatatype = firstName + age; 
+let firstName = "NC", age = 30, yearsOfStudy = 20; 
+    chkdatatype = yearsOfStudy + age + firstName; 
     document.getElementById("chdata").innerHTML = chkdatatype;
