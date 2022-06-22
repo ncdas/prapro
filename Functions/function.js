f2f593be0bb86c80c6e34f76db3f74395797c23a
NC Das <nimai.iyf@gmail.com>
2022-06-22 06:02:04 +0600

    Use function as a variable value

--- Functions/function.js
+++ Functions/function.js
@@ -1,6 +1,9 @@
-// Fahrenheit to Cehlsius
+// Use function within variable as a Variable value
 function toCelsius( fahrenheit){
     return (5/9) * (fahrenheit -32);
 }
 
 console.log(toCelsius(77));
+
+let text = "The temperature is" + toCelsius(77) + "celsius";
+console.log(text);
