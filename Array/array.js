05120bb40f00249e4eea04026a482b31555ff691
NC Das <nimai.iyf@gmail.com>
2022-06-23 02:53:11 +0600

    accessing the last element of the array

--- Array/array.js
+++ Array/array.js
@@ -4,7 +4,10 @@ const fruits = ["Mango", "Jackfruits", "Apple", "Banana"];
 let bestFruit = fruits[1]; //Accessing the index 1 element
     console.log(bestFruit); 
 
+let frt = fruits[fruits.length - 1]; // accesing last element of the arrary
+    console.log(frt);
+
 // Changing array element
 
-fruits[1] = "Orange"; // Replaced #1 index with "Orange".
-console.log(fruits);
+    fruits[1] = 'Orange';   // Replaced #1 index with "Orange"
+        console.log(fruits);
