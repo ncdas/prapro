8f623c91b2b0c31509233d45dd9dfd0858ece11f
NC Das <nimai.iyf@gmail.com>
2022-06-22 07:56:33 +0600

    String methods

---
+++ String/string.js
@@ -0,0 +1,19 @@
+// JS String Length
+
+let txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = txt.length;
+    console.log(txt.length);
+
+// String slice()
+
+let str = "Apple, Banana, Kiwi";
+let part = str.slice(7, 13);
+    console.log(part);
+    console.log(str.slice(-12, -6));
+    console.log(str.slice(-12));
+    console.log(str.slice(7));
+
+// String substr()
+
+    console.log(str.substr(7,6));
+    console.log(str.substr(7, 12));
