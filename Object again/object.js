72736e39e624a16c717dfc0a91e18e9853d8b8dc
NC Das <nimai.iyf@gmail.com>
2022-06-22 06:32:44 +0600

    declare function as a object property

--- Object again/object.js
+++ Object again/object.js
@@ -1,4 +1,11 @@
 // JS Object 
 
-const totalMarks = { bangla: 50, english: 60, math: 70};
-console.log(totalMarks["math"]);
+const totalMarks = { 
+    bangla: 50, 
+    english: 60, 
+    math: 70,
+    otherSubject : function() {
+        return this.bangla;
+    }
+};
+console.log(totalMarks.otherSubject());
