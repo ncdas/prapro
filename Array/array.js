e17501411e7b3144c7325e7da5dcaa4fc28a5cf8
NC Das <nimai.iyf@gmail.com>
2022-06-23 02:38:45 +0600

    Changed an element from the fruits array

--- Array/array.js
+++ Array/array.js
@@ -3,3 +3,8 @@
 const fruits = ["Mango", "Jackfruits", "Apple", "Banana"];
 let bestFruit = fruits[1]; //Accessing the index 1 element
     console.log(bestFruit); 
+
+// Changing array element
+
+fruits[1] = "Orange"; // Replaced #1 index with "Orange".
+console.log(fruits);
