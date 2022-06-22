135ae7d284f3f996ed73911b5205155433d21def
NC Das <nimai.iyf@gmail.com>
2022-06-23 00:42:48 +0600

    checking parseInt() method

--- Number/number.js
+++ Number/number.js
@@ -13,4 +13,11 @@ console.log(Number(true));
 let x = new Date("06/23/2022");     // Number() method on Dates
 console.log(Number(x));
 
+// parseInt()
 
+console.log(parseInt("100"));
+console.log(parseInt("-100"));
+console.log(parseInt("-10.11"));
+console.log(parseInt("10 20 30"));
+console.log(parseInt("100 years"));
+console.log(parseInt("years 100"));
