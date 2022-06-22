3672bc19a4892c5c58cad2d1e3871603ab8cbef5
NC Das <nimai.iyf@gmail.com>
2022-06-23 00:35:49 +0600

    Experiment Number() method

---
+++ Number/number.js
@@ -0,0 +1,16 @@
+// Global JS method - convert variables to number
+
+// Number()
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number("1"));
+console.log(Number("10.1"));
+console.log(Number("10,1"));
+console.log(Number("10 1"));
+console.log(Number("ncdas"));
+console.log(Number(true));
+
+let x = new Date("06/23/2022");     // Number() method on Dates
+console.log(Number(x));
+
+
