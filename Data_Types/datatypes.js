1d25103c1f47ce4b60cbe0dfa58a35d5e6269536
NC Das <nimai.iyf@gmail.com>
2022-06-24 13:44:53 +0600

    Wrap up Data_Type

--- Data_Types/datatypes.js
+++ Data_Types/datatypes.js
@@ -1,5 +1,5 @@
 
 // JS typeof
 
-let x = "Dhaka", y = 100; z = { m: "raj", se: "cox"}, h = ["mango", "sea beach"];
+let x , y , z = { m: "raj", se: "cox"}, h = ["mango", "sea beach"];
    console.log(typeof "x"  + " " + typeof "y" + " " + typeof "z" + " "+ typeof "h");
