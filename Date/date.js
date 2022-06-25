4145b8576a03c9693fa188949c696cda68f3ea4d
NC Das <nimai.iyf@gmail.com>
2022-06-25 07:40:42 +0600

    compare dates

--- Date/date.js
+++ Date/date.js
@@ -5,3 +5,21 @@ const d = new Date();
 
 const dd = new Date(2022, 04, 15, 18, 35, 59, 9); // Js count months from 0
     console.log(dd);
+    dd.setDate(35);
+    console.log(dd);
+// Compare January 2020 with January 2022
+
+let compareOutput;
+const date1 = new Date();
+    date1.setFullYear(2020, 01, 01);
+const date2 = new Date();
+    date2.setFullYear(2017, 01, 01);
+
+if (date1 > date2) {
+    compareOutput = `${date2} comes before ${date1}`;
+}
+else {
+    compareOutput =  `${date1} comes before ${date2}`;
+}
+
+    console.log(compareOutput);
