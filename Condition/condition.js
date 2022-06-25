88e5a5d0db7bf2e9b8047478a1da432379759426
NC Das <nimai.iyf@gmail.com>
2022-06-25 10:42:55 +0600

    Switch statement

--- Condition/condition.js
+++ Condition/condition.js
@@ -1,6 +1,6 @@
 // Conditional Statements
 
-
+//If else
 
 function myFunction(){
     let votable;
@@ -14,3 +14,32 @@ function myFunction(){
      
     document.getElementById("output").innerHTML = votable + " to vote";
 }
+
+// Swtich statement
+
+let day;
+
+    switch (new Date().getDay()) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3: 
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6: 
+            day = "Saturday";
+    }
+
+    console.log(day);
