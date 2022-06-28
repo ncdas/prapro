da330c196e99dbf02e3e598ef0f5f4321c314619
NC Das <nimai.iyf@gmail.com>
2022-06-28 08:25:25 +0600

    forEach funtion to access sets elements

--- sets/sets.js
+++ sets/sets.js
@@ -8,7 +8,7 @@ const letters = new Set(["a", "b", "c"]);   // created and pass an arry to a set
 
 // Add variables to a set
 const banglaLetters = new Set();
-
+let allElement = " ";
 const k = "ka";
 const kh = "kha";
 const g = "gha";
@@ -18,5 +18,15 @@ const g = "gha";
     banglaLetters.add(g);
 
     console.log(banglaLetters.size);
+
+    banglaLetters.forEach(function(value){      // forEach function to display varibale data set
+    
+        allElement = allElement + value + "</br>";; 
+
+        })
+
+    console.log(allElement);
+
+// forEach() method
 
 
