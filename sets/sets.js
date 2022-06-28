a5f72baea763f3847a8f2697016c666bc2de3726
NC Das <nimai.iyf@gmail.com>
2022-06-28 08:18:09 +0600

    Js set-set method, add elements, add variables and create a new set

---
+++ sets/sets.js
@@ -0,0 +1,22 @@
+// Js sets
+
+// new Set() method
+
+const letters = new Set(["a", "b", "c"]);   // created and pass an arry to a set method.
+    letters.add("d");       // add new element in the arry
+    console.log(letters.size);
+
+// Add variables to a set
+const banglaLetters = new Set();
+
+const k = "ka";
+const kh = "kha";
+const g = "gha";
+
+    banglaLetters.add(k);
+    banglaLetters.add(kh);
+    banglaLetters.add(g);
+
+    console.log(banglaLetters.size);
+
+
