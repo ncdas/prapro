3d338b811d950b662eb28a116d9acc37919fa8c0
NC Das <nimai.iyf@gmail.com>
2022-06-30 10:54:05 +0600

    Arrow function with and without parentheses

---
+++ Arrow_Function/arrowf.js
@@ -0,0 +1,7 @@
+// Arrow function:
+
+var hello 
+hello = (value) => "Hello " + value + " " + "How are you?";
+//hello = value => "Hello " + value; // without parentheses
+console.log(hello("Dada!"));
+
