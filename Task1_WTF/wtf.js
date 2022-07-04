8e655daea3234c54c54b900f6dd2c733a85da764
NC Das <nimai.iyf@gmail.com>
2022-07-04 11:54:26 +0600

    Draft of the issue

---
+++ Task1_WTF/wtf.js
@@ -0,0 +1,27 @@
+// Weather Temperature Forecast
+var weather_data = {
+    sunday: {
+        temp: 13,
+        rain: 20,
+    },
+    monday: {
+        temp: 16,
+        rain: 76,
+    },
+    tuesday: {
+        temp: 11,
+        rain: 45,
+    },
+    wednesday: {
+        temp: 18,
+        rain: 60,
+    },
+    thrusday: {
+        temp: 17,
+        rain: 81,
+    },
+    friday: {
+        temp: 14,
+        rain: 48,
+    },
+};
