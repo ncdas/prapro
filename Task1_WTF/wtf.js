73cc17c63b92bdeaa368f895b37670b24cc1a3f2
NC Das <nimai.iyf@gmail.com>
2022-07-13 03:05:29 +0600

    omit for loop

--- Task1_WTF/wtf.js
+++ Task1_WTF/wtf.js
@@ -37,27 +37,17 @@ for (let x in weather_data) {
     if (weather_data[x].rain > 50){
         total_Rainy_Days += 1;  
     }
-}
-console.log(total_Rainy_Days);
-
-// Hotttest Days
-for (let x in weather_data) {
+    // Hotttest Days
     if (weather_data[x].temp > hottest_temp) {
         hottest_temp = weather_data[x].temp; 
         hottest_Days.length = 0;
-        hottest_Days.push(x);
-    
+        hottest_Days.push(x); 
     }   else if (weather_data[x].temp == hottest_temp) {
             hottest_Days.push(x);
-
     }   else  {
        null;
     }
-}
-console.log(hottest_Days);
-
-// Coolest Days
-for (let x in weather_data) {
+    // Coolest Days
     if (weather_data[x].temp < coolest_temp) {
         coolest_temp = weather_data[x].temp; 
         coolest_days.length = 0; 
@@ -68,4 +58,6 @@ for (let x in weather_data) {
         null;
     }
 }
+console.log(total_Rainy_Days);
+console.log(hottest_Days);
 console.log(coolest_days);
